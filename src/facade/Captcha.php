2de7f59f82ee63350404c8ff7ffe038bed7b6551
czijian <1047672972@qq.com>
2021-12-04 14:58:16 +0800

    修正方法注解

--- src/facade/Captcha.php
+++ src/facade/Captcha.php
@@ -2,14 +2,16 @@
 
 namespace think\captcha\facade;
 
+
 use think\Facade;
+use think\Response;
 
 /**
  * Class Captcha
- * @method void check(string $code) static  校验验证码
- * @method void check4api(string $code, string $key) static 校验验证码
- * @method void create(string $config = null, bool $api = false) static 生成验证码
- * @method void create4api(string $config = null) static 生成验证码配置信息
+ * @method Response create(string $config = null, bool $api = false) static 生成验证码
+ * @method array create4api(string $config = null) static 生成验证码配置信息
+ * @method bool check(string $code) static  校验验证码
+ * @method bool check4api(string $code, string $key) static 校验验证码
  * @package think\captcha\facade
  * @mixin \think\captcha\Captcha
  */
